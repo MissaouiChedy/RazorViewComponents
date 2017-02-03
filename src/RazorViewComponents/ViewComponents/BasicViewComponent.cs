28667523ea76569ed5b4a11aa6db8c34694843ca
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-02-03 23:46:01 +0100

    added examples

---
+++ src/RazorViewComponents/ViewComponents/BasicViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorViewComponents.ViewComponents
+{
+    public class Holder 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BasicViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int id, string name)
+        {
+            
+            ViewData["SomeData"] = "This string is passed via ViewData";
+            /*
+             * This returns a ViewResult that represents
+             * the rendering of the cshtml template related
+             * to this view component
+             */
+            return View(new Holder{ Id = id, Name= name });
+        }
+    }
+}
